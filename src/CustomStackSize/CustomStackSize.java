502211826c3446ee7a59e6a8544a5dc8f2d5b77c
BuildTools <unconfigured@null.spigotmc.org>
2020-07-29 17:49:19 +1000

    initial commit

---
+++ src/CustomStackSize/CustomStackSize.java
@@ -0,0 +1,15 @@
+package CustomStackSize;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomStackSize extends JavaPlugin {
+    @java.lang.Override
+    public void onEnable() {
+        getLogger().info("CustomStackSize has been enabled.")
+    }
+
+    @java.lang.Override
+    public void onDisable() {
+        getLogger().info("CustomStackSize has been disabled.")
+    }
+}
